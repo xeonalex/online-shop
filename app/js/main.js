8c367538304fc66227018a6d17881fdbdf47bb19
Sasha Saulyak <lux1995@yandex.ru>
2016-02-29 01:23:01 +0200

    Fixed some css bugs. Rewrote js

--- app/js/main.js
+++ app/js/main.js
@@ -1,154 +1,135 @@
 'use strict';
 $(document).ready(function(){
-
-	$('.accordeon__toggle').on('click',function(e){
-		var $this=$(this),
-			parentAccordeon=$this.closest('.accordeon'),
-			parentBlock=$this.closest('.accordeon__li'),
-			toggles=parentAccordeon.find('.accordeon-toggle'),
-			currentContent=parentBlock.find('.accordeon__content'),
-			content=parentAccordeon.find('.accordeon__content');
-
-		if(!$this.hasClass('accordeon_active')) {
-			toggles.removeClass('accordeon_active');
-			content.slideUp('slow');
-			$this.addClass('accordeon_active');
-			currentContent.stop(true,true).slideDown('slow');
-		} else {
-			currentContent.stop(true,true).slideUp('slow');
-			$this.removeClass('accordeon_active');
-			toggles.removeClass('accordeon_active');
-		}
-
-	});
-
 	var ResetLinkModule = (function(){
-
 		function cleaning(link) {
-				var parent=link.closest('.accordeon__content');
-				console.log(parent);
-				var checkbox=parent.find('input:checkbox');
-				checkbox.removeAttr("checked");
+				link.closest('.accordeon__content')
+				.find('input:checkbox')
+				.removeAttr("checked");
 		}
 		function listener(link){
 			link.on('click',function(event){
-				var $this=$(this);
 				event.preventDefault ? event.preventDefault() : (event.returnValue=false);
-				cleaning($this);
-
+				cleaning($(this));
 			});
-
 		}
 		return {
 			reset: listener
 		}
-
 	})();
 
-
-
-
+	var AccordeonModule = (function(){
+		function listeners(){
+			$('.accordeon__toggle').on('click', function(e){
+				ToggleBlock($(this));
+			});
+		}
+		function ToggleBlock($this){
+			var parentAccordeon=$this.closest('.accordeon'),
+				parentBlock=$this.closest('.accordeon__li'),
+				togglesActive=parentAccordeon.find('.accordeon_active'),
+				togglesContent=parentAccordeon.find('.accordeon_active + .accordeon__content'),
+				currentContent=parentBlock.find('.accordeon__content');
+			if(!$this.hasClass('accordeon_active')) {
+				togglesContent.stop(true, true).slideUp('slow');
+				togglesActive.removeClass('accordeon_active');
+				$this.addClass('accordeon_active');
+				currentContent.stop(true,true).slideDown('slow');
+			} else {
+				currentContent.stop(true,true).slideUp('slow');
+				$this.removeClass('accordeon_active');
+			}
+		}
+		return {
+			follow: listeners
+		}
+	})();
 
 /* слайдер цен */
 
-jQuery("#slider").slider({
-	min: 0,
-	max: 1000,
-	values: [0,1000],
-	range: true,
-	stop: function(event, ui) {
-		jQuery("input#minCost").val(jQuery("#slider").slider("values",0));
-		jQuery("input#maxCost").val(jQuery("#slider").slider("values",1));
+	var asideFiltersModule = (function(){
+		function getValues() {
+			var valueMin=jQuery("input#minCost").val();
+			var valueMax=jQuery("input#maxCost").val();
+			return {
+				valueMin: valueMin,
+				valueMax: valueMax
+			}
+		}
 
-    },
-    slide: function(event, ui){
-		jQuery("input#minCost").val(jQuery("#slider").slider("values",0));
-		jQuery("input#maxCost").val(jQuery("#slider").slider("values",1));
-    }
-});
+		function init (){
+			jQuery("#slider").slider({
+				min: 0,
+				max: 1000,
+				values: [0,1000],
+				range: true,
+				stop: function(event, ui) {
+					jQuery("input#minCost").val(jQuery("#slider").slider("values",0));
+					jQuery("input#maxCost").val(jQuery("#slider").slider("values",1));
+			    },
+			    slide: function(event, ui){
+					jQuery("input#minCost").val(jQuery("#slider").slider("values",0));
+					jQuery("input#maxCost").val(jQuery("#slider").slider("values",1));
+			    }
+			});
+			setUpListeners();
+		}
 
-jQuery("input#minCost").change(function(){
+		function setUpListeners(){
+			jQuery("input#minCost").change(function(){
+				getValues();
+			    if(parseInt(valueMin) > parseInt(valueMax)){
+					valueMin = valueMax;
+					$(this).val(valueMin);
+				}
+				$(this).siblings("#slider").slider("values",0,valueMin);
+			});
 
-	var value1=jQuery("input#minCost").val();
-	var value2=jQuery("input#maxCost").val();
+			jQuery("input#maxCost").change(function(){
+				getValues();
+				if (valueMax > 1000) { valueMax = 1000; jQuery("input#maxCost").val(1000)}
 
-    if(parseInt(value1) > parseInt(value2)){
-		value1 = value2;
-		jQuery("input#minCost").val(value1);
-	}
-	jQuery("#slider").slider("values",0,value1);
-});
+				if(parseInt(valueMin) > parseInt(valueMax)){
+					valueMax = valueMin;
+					$(this).val(valueMax);
+				}
+				$(this).siblings("#slider").slider("values",1,valueMax);
+			});
+				// фильтрация ввода в поля
+			jQuery('input#maxCost,input#minCost').keypress(function(event){
+				var key, keyChar;
+				if(!event) var event = window.event;
 
+				if (event.keyCode) key = event.keyCode;
+				else if(event.which) key = event.which;
 
-jQuery("input#maxCost").change(function(){
-
-	var value1=jQuery("input#minCost").val();
-	var value2=jQuery("input#maxCost").val();
-
-	if (value2 > 1000) { value2 = 1000; jQuery("input#maxCost").val(1000)}
-
-	if(parseInt(value1) > parseInt(value2)){
-		value2 = value1;
-		jQuery("input#maxCost").val(value2);
-	}
-	jQuery("#slider").slider("values",1,value2);
-});
-
-
-
-// фильтрация ввода в поля
-	jQuery('input#maxCost,input#minCost').keypress(function(event){
-		var key, keyChar;
-		if(!event) var event = window.event;
-
-		if (event.keyCode) key = event.keyCode;
-		else if(event.which) key = event.which;
-
-		if(key==null || key==0 || key==8 || key==13 || key==9 || key==46 || key==37 || key==39 ) return true;
-		keyChar=String.fromCharCode(key);
-
-		if(!/\d/.test(keyChar))	return false;
-
-	});
-
-
-	$('.filter-colors__block').on('click',function(event) {
-		event.preventDefault ? event.preventDefault() : (event.returnValue=false);
-		var $this=$(this),
-			Parent=$this.closest('.filter-colors'),
-			ColorsBlocks=Parent.find('.filter-colors__block_active');
-			console.log(Parent);
-			ColorsBlocks.removeClass('filter-colors__block_active');
-			$(this).addClass('filter-colors__block_active');
-	})
-
-
-
-		/*** СЛАЙДЕР фото **/
-		$('.product-wrap__preview-list').find('.product-wrap__preview-li').on('click',function(){
-			var currentSlider=$(this),
-			 	sliderList=currentSlider.parent('.product-wrap__preview-list'),
-			 	sliderBlock=currentSlider.closest('.product-wrap__preview'),
-			  	link = currentSlider.find('img').attr('src'),
-			  	big_image=sliderBlock.find(".product-wrap__big-image"),
-			  	big_image_link=big_image.attr('src');
-
-			  	sliderList.find('.product-wrap__preview-li_active').removeClass('product-wrap__preview-li_active');
-			  	currentSlider.addClass('product-wrap__preview-li_active');
-			  	big_image.stop(true,true).fadeOut('100',function(){
-				  	$(this).attr("src",link);
-				  	big_image.stop(true,true).fadeIn('100');
-				});
-		})
-
-		$('.content__view-mode-list').find('[data-mode]').on('click',function(event){
-			event.preventDefault ? event.preventDefault() : (event.returnValue=false);
-			var currentModeLink=$(this),
-				currentMode=currentModeLink.attr('data-mode'),
+				if(key==null || key==0 || key==8 || key==13 || key==9 || key==46 || key==37 || key==39 ) return true;
+				keyChar=String.fromCharCode(key);
+				if(!/\d/.test(keyChar))	return false;
+			});
+		}
+		return {
+			init: init
+		}
+	})();
+	var productPageModule = (function(){
+		function setUpListeners(){
+			$('.filter-colors__block').on('click',function(event) {
+				event.preventDefault ? event.preventDefault() : (event.returnValue=false);
+				colorsFilter($(this));
+			});
+			$('.product-wrap__preview-list').find('.product-wrap__preview-li').on('click',function(){
+				PhotoSlider($(this));
+			});
+			$('.content__view-mode-list').find('[data-mode]').on('click',function(event){
+				event.preventDefault ? event.preventDefault() : (event.returnValue=false);
+				displayMode($(this));
+			})
+		}
+					// ПЕРЕКЛЮЧАТЕЛЬ DISPLAY MODE
+		function displayMode(currentModeLink) {
+			var currentMode=currentModeLink.attr('data-mode'),
 				productList=$('.content__product-list'),
 				productWrap=$('.content__product-wrap');
-
-
 			var modes = {
 				'line': ['product-wrap_line','product-list_line'],
 				'table':['product-wrap_table','product-list_table'],
@@ -156,7 +137,6 @@ jQuery("input#maxCost").change(function(){
 			};
 				productList.attr('class','content__product-list');
 				productWrap.attr('class','content__product-wrap');
-
 			switch (currentMode){
 				case 'line': productList.addClass(modes.line[1]);
 							 productWrap.addClass(modes.line[0]);
@@ -168,11 +148,38 @@ jQuery("input#maxCost").change(function(){
 							 productWrap.addClass(modes.tile[0]);
 							 break;
 			}
-		})
+		}
+		function PhotoSlider(currentSlider) {
+			var sliderList=currentSlider.parent('.product-wrap__preview-list'),
+			 	sliderBlock=currentSlider.closest('.product-wrap__preview'),
+			  	link = currentSlider.find('img').attr('src'),
+			  	big_image=sliderBlock.find(".product-wrap__big-image"),
+			  	big_image_link=big_image.attr('src');
 
+			  	sliderList.find('.product-wrap__preview-li_active').removeClass('product-wrap__preview-li_active');
+			  	currentSlider.addClass('product-wrap__preview-li_active');
+			  	big_image.stop(true,true).fadeOut('100',function(){
+				  	$(this).attr("src",link);
+				  	big_image.stop(true,true).fadeIn('100');
+				});
+		}
+		function colorsFilter($this) {
+			var Parent=$this.closest('.filter-colors'),
+				ColorsBlocks=Parent.find('.filter-colors__block_active');
+				ColorsBlocks.removeClass('filter-colors__block_active');
+				$this.addClass('filter-colors__block_active');
+		}
+		return {
+			setup : setUpListeners
+		}
+	})();
 			// ВЫЗОВ модулей
-		ResetLinkModule.reset($('.filter-reset-link'));
-
+		if($('.filter-reset-link').length>=1) {
+			ResetLinkModule.reset($('.filter-reset-link'));
+		}
+		AccordeonModule.follow();
+		asideFiltersModule.init();
+		productPageModule.setup();
 			// Активация необходимых возможностей: columnizer, select2
 		$('.site-info__text').columnize({columns: 2 });
 		$('.sorter__select').select2().css('width','143');
